9f9fdf9f852ec759084cce10c921da47edd39f39
Rutvik599 <126308040+Rutvik599@users.noreply.github.com>
2023-07-19 19:15:23 +0530

    Practical 1.1

--- src/practical1.kt
+++ src/practical1.kt
@@ -1,5 +1,18 @@
-fun main(args: Array<String>) {
-    var number1 = 10
-    var num2 = 15
-    print("Hello")
+fun main() {  //practical 1.1 
+    var number1 = 10;
+    var number2 = 20;
+    // Swapping Two variable Without using third variable
+    println("Before Swapping the Variable a = " + number1 + " and b = " + number2)
+   	number1 = number1 + number2
+    number2 = number1 - number2
+    number1 = number1 - number2
+    println("Before Swapping the Variable a = " + number1 + " and b = " + number2)
+    
+    // Swapping two variable with using third variable 
+    println("*************************************")
+    println("Before Swapping the Variable a = " + number1 + " and b = " + number2)
+    var temp = number1
+    number1 = number2
+    number2 = temp
+    println("Before Swapping the Variable a = " + number1 + " and b = " + number2)
 }
