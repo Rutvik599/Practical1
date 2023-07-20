b5cabc33a8f7b47a34d6582c15b95724f6fa9bad
student <student@gnu.ac.in>
2023-07-20 12:33:08 +0530

    practical 1.6

---
+++ src/Prac_6.kt
@@ -0,0 +1,16 @@
+fun main(){
+    math(6,3,"+")
+    math(5,2,"-")
+    math(6,2,"*")
+    math(10,2,"/")
+}
+
+fun math(number1:Int,number2:Int,choice:String){
+    when(choice){
+        "+" -> println("Sum : " +(number1+number2))
+        "-" -> println("Sub : " + (number1-number2))
+        "*" -> println("Mul : " + (number1*number2))
+        "/" -> println("Div : " + (number1/number2))
+        else -> print("Enter the valid !")
+    }
+}
