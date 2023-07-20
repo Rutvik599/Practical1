5e9607eaba85583110493b5de391b2cb8f7c783c
student <student@gnu.ac.in>
2023-07-20 12:03:40 +0530

    practical 1.3

---
+++ src/Prac_3.kt
@@ -0,0 +1,21 @@
+fun main(){
+    println("student Enrollment No. :")
+    val eno = readLine()!!
+    println("student Name :")
+    val name1 = readLine()!!
+    println("student Brach :")
+    val branch1 = readLine()!!
+    println("student Class :")
+    val class1 = readLine()!!
+    println("student Batch :")
+    val batch1 = readLine()!!
+    println("student College Name :")
+    val clg = readLine()!!
+    println("student University Name :")
+    val uny = readLine()!!
+    println("student Age :")
+    val age = readLine()!!
+    println("Students Data : ")
+    println(" Enrollment No. $eno \n Name : $name1 \n Branch : $branch1 \n Class : $class1 \n Batch : $batch1 \n College : $clg \n University : $uny")
+
+}
