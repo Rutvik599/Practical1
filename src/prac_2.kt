a3c3f24ce4d3d166316b225851cef69570754459
student <student@gnu.ac.in>
2023-07-20 11:47:12 +0530

    practical 1.2

---
+++ src/prac_2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var number1 = 10
+    var dob = number1.toDouble()
+    var string1 = "5999"
+    var number2 = string1.toInt()
+    var dob1 = string1.toDouble()
+    println("Integer to the Double " +dob)
+    println("String to Integer " +number2)
+    println("String to Double " +dob1)
+}
