2ca7d89d9839692272e0994f0d1b468a365fca52
student <student@gnu.ac.in>
2023-07-20 11:31:07 +0530

    practical 1.1

---
+++ src/Prac_1.kt
@@ -0,0 +1,21 @@
+fun main() {  //practical 1.1
+    var number1:Int = 10
+    var number2:Float = 10.52F
+    var chara:Char = 'a'
+    var string1:String = "Hello World"
+    var bool:Boolean = true
+    var doub:Double = 10.232323132212
+    var lg:Long = 1021203143531
+    var srt:Short = -2
+    var byt:Byte = 127
+    println("Int is " +number1)
+    println("Float is " +number2)
+    println("Char is " +chara)
+    println("String is " +string1)
+    println("Boolean is " +bool)
+    println("Double is " +doub)
+    println("Long is " +lg)
+    println("Sort is " +srt)
+    println("byt is " +byt)
+
+}
