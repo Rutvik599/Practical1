3b33ea3c684e9da33c13852c4910e850600a2a4e
Rutvik599 <126308040+Rutvik599@users.noreply.github.com>
2023-07-27 22:32:24 +0530

    Practical 1.9

---
+++ src/Prac_9.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var arr1 :ArrayList<Int> = ArrayList()
+    for(i in 0 until 5 ){
+        print("a[$i] : ")
+        var temp = readLine()!!.toInt()
+        arr1.add(temp)
+    }
+    println("\nThe Maximum number is : ${arr1.max()}")
+
+}
